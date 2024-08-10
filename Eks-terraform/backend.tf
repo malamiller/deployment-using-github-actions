273633e410d9c09813f274ce27c5302ca6ae0d8a
malamiller <malawimiller2015@gmail.com>
2024-08-10 23:47:41 +0000

    changes

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mario123bucket" # Replace with your actual S3 bucket name
+    bucket = "swiggy-app-109" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }
